af0881ffc2a0e46635ce8e85f884692f69153631
Wheslley <wheslleyny@gmail.com>
2017-11-20 17:23:50 -0200

    Classes

--- src/br/com/sisbrava/teste/TestePessoaPossuiClasse.java
+++ src/br/com/sisbrava/teste/TestePessoaPossuiClasse.java
@@ -2,34 +2,38 @@ package br.com.sisbrava.teste;
 
 import java.util.List;
 
-import br.com.sisbrava.bean.Classes;
-import br.com.sisbrava.bean.Pessoa;
 import br.com.sisbrava.bean.PessoaPossuiClasse;
+import br.com.sisbrava.bean.Usuario;
 import br.com.sisbrava.interfaces.mensagem.ITables;
-import br.com.sisbrava.repository.ClassesRepository;
 import br.com.sisbrava.repository.PessoaPossuiClasseRepository;
-import br.com.sisbrava.repository.PessoaRepository;
+import br.com.sisbrava.repository.UsuarioRepository;
 
 public class TestePessoaPossuiClasse {
 
 	public static void main(String[] args) {
 
-		Pessoa pessoa = (Pessoa) PessoaRepository.getInstance().getSelectOneObject(1);
-		
-		Classes classe = (Classes) ClassesRepository.getInstance().getSelectOneObject(2);
-		PessoaPossuiClasse ppClasse = new PessoaPossuiClasse();
-		ppClasse.setPessoa(pessoa);
-		ppClasse.setClasse(classe);
-		ppClasse.setStatus(2);
-		
-		PessoaPossuiClasseRepository.getInstance().insert(ppClasse);
+		/*
+		 * Pessoa pessoa = (Pessoa)
+		 * PessoaRepository.getInstance().getSelectOneObject(1);
+		 * 
+		 * Classes classe = (Classes)
+		 * ClassesRepository.getInstance().getSelectOneObject(2); PessoaPossuiClasse
+		 * ppClasse = new PessoaPossuiClasse(); ppClasse.setPessoa(pessoa);
+		 * ppClasse.setClasse(classe); ppClasse.setStatus(2);
+		 * 
+		 * PessoaPossuiClasseRepository.getInstance().insert(ppClasse);
+		 */
+
+		Usuario usuario = (Usuario) UsuarioRepository.getInstance().getSelectOneObject(1);
 
 		List<Object> lista = (List<Object>) PessoaPossuiClasseRepository.getInstance()
 				.selectMultiplusObjects(ITables.PESSOA_POSSUI_CLASSE);
 
 		for (Object obj : lista) {
 			PessoaPossuiClasse ppClass = (PessoaPossuiClasse) obj;
-			System.out.println(ppClass.toString());
+			if (ppClass.getStatus() == 2) {
+				System.out.println(ppClass.toString());
+			}
 		}
 
 	}
